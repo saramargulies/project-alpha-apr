73941f16237c6ff65735f6bd3a0ac7af6b9ee739
Sara <saraaustenm@gmail.com>
2023-04-24 15:43:46 -0600

    Feature 12 complete

---
+++ .history/tasks/admin_20230424153914.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from tasks.models import Task
+
+
+@admin.register(Task):
---
+++ .history/tasks/admin_20230424154303.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from tasks.models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "start_date", "due_date", "is_completed", "project", "assignee")
