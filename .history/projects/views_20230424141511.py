bbb30609cb24a9cb71fd4fcf4bdf85b75258a7f8
Sara <saraaustenm@gmail.com>
2023-04-24 14:18:52 -0600

    Feature 8 complete

--- .history/projects/views_20230424141511.py
+++ .history/projects/views_20230424141511.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
 from projects.models import Project
-from django.contrib.auth.decorators import login_required
 
-
-@login_required(redirect_field_name="login")
+@login_required(require)
 def list_projects(request):
-    projects = Project.objects.filter(owner=request.user)
+    projects = Project.objects.all()
     context = {"projects": projects}
     return render(request, "projects/list.html", context)
--- .history/projects/views_20230424141615.py
+++ .history/projects/views_20230424141615.py
@@ -5,6 +5,6 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(redirect_field_name="login")
 def list_projects(request):
-    projects = Project.objects.filter(owner=request.user)
+    projects = Project.objects..filter(owner=request.user)
     context = {"projects": projects}
     return render(request, "projects/list.html", context)
